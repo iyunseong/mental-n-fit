2612132d2778d5529eed5cd4387e4bee892229d4
yunseong <yunseong0717@gmail.com>
2025-08-19 19:47:57 +0900

    refactor(meal-form wrapper): remove any via ComponentProps<typeof Legacy>

--- src/components/dashboard/MealLogForm.tsx
+++ src/components/dashboard/MealLogForm.tsx
@@ -1,7 +1,11 @@
 'use client'
 import React from 'react'
+import type { ComponentProps } from 'react'
 import Legacy from '@/components/forms/MealLogForm'
-export default function MealLogForm(props: any){
+
+type Props = ComponentProps<typeof Legacy>
+
+export default function MealLogForm(props: Props){
   return <Legacy {...props} />
 }
 
